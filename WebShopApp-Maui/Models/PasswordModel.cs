db965f4f92b720fe089be626c0be957b46b25152
dalya <dalyalhamami@gmail.com>
2024-08-30 13:33:11 +0200

    adding change password and order history to user page

---
+++ WebShopApp-Maui/Models/PasswordModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebShopApp_Maui.Models;
+
+public class PasswordModel
+{
+    public int UserId { get; set; }
+    public string OldPassword { get; set; }
+    public string Password { get; set; }
+    public string ConfirmPassword { get; set; }
+}
